405aa12162d28bc320ae626529e852b94cabafac
burak-akturk06 <burak.akturk174@gmail.com>
2026-06-07 23:34:07 +0200

    feat: add integer printing ability
    - Add intToString helper function to print %i and %d cases

--- print.c
+++ print.c
@@ -2,19 +2,17 @@
 #include <stdarg.h>
 
 #define MAX_ARRAY_LENGTH 1024
+#define ASCII_CHAR_INT_DIFF 48
 
 static int printFinalArray(char *text, int lengthOfArray);
 static int countStringLength(char *arr);
+static int intToString(int integer, int indexOfText, char *arr);
 
 int print(char *arr, ...)
 {
-/*     va_list ap;
+    va_list ap;
     va_start(ap, arr);
-    va_arg(ap, char);
-    va_end(ap); 
-    NOT IMPLEMENTED
-    */
-
+    // va_arg(ap, char);
     int indexOfText = 0;
     int lengthOfArray = 0;
     char *ptrArray = arr;
@@ -44,13 +42,29 @@ int print(char *arr, ...)
                     text[indexOfText] = '%';
                     indexOfText += 1;
                     break;
-                
+
+                case 'i':
+                {
+                    int integer = va_arg(ap, int);
+                    indexOfText = intToString(integer, indexOfText, text);
+                    break;
+                }
+
+                case 'd':
+                {
+                    int integer = va_arg(ap, int);
+                    indexOfText = intToString(integer, indexOfText, text);
+                    break;
+                }
+
                 default:
                     break;
             }
         }
         ptrArray += 1;
     }
+  
+    va_end(ap);
     text[indexOfText] = '\0';
     lengthOfArray = countStringLength(text);
     return printFinalArray(text, lengthOfArray);
@@ -81,9 +95,49 @@ static int countStringLength(char *arr)
     return stringLength;
 }
 
+static int intToString(int integer, int indexOfText, char *arr)
+{
+    int remainder = 0;
+    char c;
+    char tempArr[32];
+    int index = 0;
+    if(integer == 0)
+    {
+        c = '0';
+        arr[indexOfText] = c;
+        indexOfText += 1;
+        return indexOfText;
+    }
 
+    if(integer < 0)
+    {
+        arr[indexOfText] = '-';
+        indexOfText += 1;
+        integer = -integer;
+    }
+    while (integer >= 1)
+    {
+        remainder = integer % 10;
+        c = remainder + ASCII_CHAR_INT_DIFF;
+        tempArr[index] = c;
+        index += 1;
+        integer /= 10;
+    }
+    tempArr[index] = '\0';
 
+    for (int i = 0, j = index - 1; i < j; i++, j--)
+    {
+        char temp = tempArr[j];
+        tempArr[j] = tempArr[i];
+        tempArr[i] = temp;
+    }
+    
+    for(int i = 0; i < index; i++)
+    {
+        arr[indexOfText] = tempArr[i];
+        indexOfText += 1;
+    }
 
-
-
-
+    return indexOfText;
+    
+}
